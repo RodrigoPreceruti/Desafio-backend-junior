d07e71ea0430f4f803f58a98350b8dad6b4c22b0
Rodrigo <rodrigoprecerutti28@gmail.com>
2024-02-14 16:04:58 -0300

    first commit

---
+++ src/main/java/com/example/desafiobackendjunior/DesafiobackendjuniorApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiobackendjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobackendjuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobackendjuniorApplication.class, args);
+	}
+
+}
